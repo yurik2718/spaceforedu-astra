2d8855fe700efdd60d06d03b0dfa4866f29bc355
yurik2718 <andrei.iurik@gmail.com>
2026-04-20 02:25:00 +0300

    init

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://spaceforedu.com",
+  output: "static",
+  integrations: [react()],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en", "ru"],
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: true,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
